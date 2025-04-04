0aedcad4eeaf543adbc8a73ba288512994375a25
wsxiaolin <xiegushi2022@outlook.com>
2025-04-04 12:30:45 +0800

    one more test

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -14,27 +14,26 @@ export async function getData(path: String, body: any) {
       "Content-Type": "application/json",
       "x-API-Token": localStorage.getItem("token") || undefined,
       "x-API-AuthCode": localStorage.getItem("authCode") || undefined,
-      "Origin": window.origin,
+      Origin: window.origin,
     },
-  })
-    .then((response) => {
-      window.$message.destroyAll();
-      if (!response.ok) {
-        return response.json().then(() => {
-          Emitter.emit("error", "无法与服务器通讯，请稍候再试", 3);
-        });
-      }
-      return response.json().then((data) => {
-        if (data.Status == 403 && localStorage.getItem("loginStatus") !== "true") {
-          Emitter.emit("loginRequired");
-          Emitter.emit("error", translateErrorMessage(data.Message), 3);
-        } else if (data.Status !== 200) {
-          noMessagesPath.some((p) => path === p) ||
-            Emitter.emit("error", translateErrorMessage(data.Message), 3);
-        }
-        return data;
+  }).then((response) => {
+    window.$message.destroyAll();
+    if (!response.ok) {
+      return response.json().then(() => {
+        Emitter.emit("error", "无法与服务器通讯，请稍候再试", 3);
       });
-    })
+    }
+    return response.json().then((data) => {
+      if (data.Status == 403 && localStorage.getItem("loginStatus") !== "true") {
+        Emitter.emit("loginRequired");
+        Emitter.emit("error", translateErrorMessage(data.Message), 3);
+      } else if (data.Status !== 200) {
+        noMessagesPath.some((p) => path === p) ||
+          Emitter.emit("error", translateErrorMessage(data.Message), 3);
+      }
+      return data;
+    });
+  });
 }
 
 export async function login(arg1: String | null, arg2: String | null, is_token = false) {
@@ -42,7 +41,7 @@ export async function login(arg1: String | null, arg2: String | null, is_token =
   Emitter.emit("loading", "正在与服务器通信...", 50);
   let username = is_token ? null : arg1;
   let password = is_token ? null : arg2;
-  let header = { "Content-Type": "application/json" };
+  let header = { "Content-Type": "application/json", Origin: window.origin };
   if (is_token && arg1 && arg2) {
     // @ts-ignore
     header["x-API-Token"] = arg1;
@@ -61,25 +60,24 @@ export async function login(arg1: String | null, arg2: String | null, is_token =
       },
     }),
     headers: header,
-  })
-    .then(async (response) => {
-      window.$message.destroyAll();
-      if (!response.ok) {
-        return response.json().then(() => {
-          Emitter.emit("error", "无法与服务器通讯，请稍候再试", 3);
-        });
-      }
-      return response.json().then((data) => {
-        if (data.Status === 403) {
-          Emitter.emit("error", "用户名或密码错误", 3);
-          throw "不必在意的错误";
-        } else if (data.Status !== 200) {
-          Emitter.emit("error", translateErrorMessage(data.Message), 3);
-        }
-        window.$message.destroyAll();
-        password && localStorage.setItem("loginStatus", "true");
-        password && Emitter.emit("success", "登录成功", 1);
-        return data;
+  }).then(async (response) => {
+    window.$message.destroyAll();
+    if (!response.ok) {
+      return response.json().then(() => {
+        Emitter.emit("error", "无法与服务器通讯，请稍候再试", 3);
       });
-    })
+    }
+    return response.json().then((data) => {
+      if (data.Status === 403) {
+        Emitter.emit("error", "用户名或密码错误", 3);
+        throw "不必在意的错误";
+      } else if (data.Status !== 200) {
+        Emitter.emit("error", translateErrorMessage(data.Message), 3);
+      }
+      window.$message.destroyAll();
+      password && localStorage.setItem("loginStatus", "true");
+      password && Emitter.emit("success", "登录成功", 1);
+      return data;
+    });
+  });
 }
