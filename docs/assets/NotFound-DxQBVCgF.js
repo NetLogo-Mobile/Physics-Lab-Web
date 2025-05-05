4684f028ef4d06e49e19dd22385d79e98c5af766
wsxiaolin <xiegushi2022@outlook.com>
2025-05-05 12:29:26 +0800

    new build

--- docs/assets/NotFound-DxQBVCgF.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index-_4D6VQpz.js";import{I as r,J as o,H as n}from"./vue-Bz9MZDTH.js";import"./domPurify-CocPUCx9.js";const s=""+new URL("messages/Message-Default.png",import.meta.url).href,a={};function l(m,e){return n(),r("div",null,e[0]||(e[0]=[o("h1",null,"404 not found",-1),o("img",{src:s},null,-1)]))}const i=t(a,[["render",l]]);export{i as default};
